4dff7cf889b02fca45fc0d6bbf9c4ecd6c5e1407
Erik R <38637170+longarms07@users.noreply.github.com>
2022-11-08 08:19:30 -0800

    Refactored movement into its own script, made it physics based

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovableObject : MonoBehaviour
+{
+    public BoxCollider2D boxCollider2D;
+    public Rigidbody2D rigidBody2D;
+    public float moveSpeed = 2.5f;
+
+    private bool moving = false;
+    private Vector3 target;
+    private Vector3 prevPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 newPos = Vector3.MoveTowards(this.transform.localPosition, target, 0.05f);
+            rigidBody2D.MovePosition(newPos);
+            if (newPos == target)
+            {
+                moving = false;
+            }
+        }
+    }
+
+
+    #region Movement
+
+    private void Move(Vector3 dirVector)
+    {
+        if (!moving)
+        {
+            float width = boxCollider2D.size.x;
+            float height = boxCollider2D.size.y;
+            dirVector.x *= width;
+            dirVector.y *= height;
+            prevPos = this.transform.localPosition;
+            target = this.transform.localPosition + dirVector;
+            moving = true;
+        }
+    }
+
+    public void MoveUp()
+    {
+        Move(new Vector3(0, moveSpeed, 0));
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -moveSpeed, 0));
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-moveSpeed, 0, 0));
+    }
+
+    public void MoveRight()
+    {
+        Move(new Vector3(moveSpeed, 0, 0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("boom!");
+        moving = true;
+        target = prevPos;
+    }
+
+    #endregion
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(MovableObject))]
 public class PlayerController : MonoBehaviour
 {
 
-    public BoxCollider2D boxCollider2D;
-
-    private bool moving = false;
-    private Vector3 target;
+    public MovableObject movable;
 
     // Start is called before the first frame update
     void Start()
@@ -20,57 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (moving)
-        {
-            Vector3 newPos = Vector3.MoveTowards(this.transform.localPosition, target, 0.01f);
-            this.transform.localPosition = newPos;
-            if (newPos == target) moving = false;
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.W)) MoveUp();
-            else if (Input.GetKeyDown(KeyCode.A)) MoveLeft();
-            else if (Input.GetKeyDown(KeyCode.S)) MoveDown();
-            else if (Input.GetKeyDown(KeyCode.D)) MoveRight();
-        }
+        if (Input.GetKeyDown(KeyCode.W)) movable.MoveUp();
+        else if (Input.GetKeyDown(KeyCode.A)) movable.MoveLeft();
+        else if (Input.GetKeyDown(KeyCode.S)) movable.MoveDown();
+        else if (Input.GetKeyDown(KeyCode.D)) movable.MoveRight();
 
 
     }
 
 
-    #region Movement
-
-    private void Move(Vector3 dirVector)
-    {
-        float width = boxCollider2D.size.x;
-        float height = boxCollider2D.size.y;
-        dirVector.x *= width;
-        dirVector.y *= height;
-        target = this.transform.localPosition + dirVector;
-        moving = true;
-    }
-
-    public void MoveUp()
-    {
-        Move(new Vector3(0, 2.5f, 0));
-    }
-
-    public void MoveDown()
-    {
-        Move(new Vector3(0, -2.5f, 0));
-    }
-
-    public void MoveLeft()
-    {
-        Move(new Vector3(-2.5f, 0, 0));
-    }
-
-    public void MoveRight()
-    {
-        Move(new Vector3(2.5f, 0, 0));
-    }
-
-    #endregion
+    
 
 
 }
