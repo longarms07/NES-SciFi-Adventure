2a2437184caeae741053851dc1cf92adda3a6177
Erik R <38637170+longarms07@users.noreply.github.com>
2022-12-09 20:25:49 -0800

    Added default positions/rotations for swords
    Maybe it'd be better to handle these through animations?

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -17,6 +17,18 @@ public class Sword : MonoBehaviour
     private bool stabbing;
     public float stabTimeElapsed;
 
+    public Vector3 upLocalPosition = new Vector3(0.0f, 0.1f, 0.0f);
+    public Vector3 downLocalPosition = new Vector3(0.0f, -0.1f, 0.0f);
+    public Vector3 leftLocalPosition = new Vector3(-0.1f, 0.0f, 0.0f);
+    public Vector3 rightLocalPosition = new Vector3(0.1f, 0.0f, 0.0f);
+
+    public Quaternion upLocalRotation = new Quaternion(0.0f, 0.0f, 0.7f, 0.7f);
+    public Quaternion downLocalRotation = new Quaternion(0.0f, 0.0f, -0.7f, 0.7f);
+    public Quaternion leftLocalRotation = new Quaternion(0.0f, 0.0f, 1.0f, 0.0f);
+    public Quaternion rightLocalRotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +43,8 @@ public class Sword : MonoBehaviour
             stabTimeElapsed += Time.deltaTime;
             if (stabTimeElapsed >= stabDuration) Sheath();
         }
+
+        Debug.Log(this.transform.localPosition);
     }
 
     public void Stab()
